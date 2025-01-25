95437c029f2e350d7b958c13183fb06f17e6e455
Wonky's World <wonkysworld2024@gmail.com>
2025-01-25 20:07:45 +0000

    Add files via upload

---
+++ config/shared.lua
@@ -0,0 +1,42 @@
+return {
+    shellUndergroundOffset = 50.0,
+
+    apartmentOptions = {
+        {
+            interior = 'DellPerroHeightsApt4',
+            label = 'Fantastic Plaza',
+            description = 'Enjoy ocean views far away from tourists and bums on Del Perro Beach.',
+            enter = vec3(-1447.35, -537.84, 34.74)
+        },
+        {
+            interior = 'DellPerroHeightsApt7',
+            label = 'Tinsel Towers',
+            description = 'Luxury Del Perro Heights apartment complex! For all you voyeurs out there!',
+            enter = vec3(-1447.35, -537.84, 34.74)
+        },
+        {
+            interior = '4IntegrityWayApt30',
+            label = 'Integrity Way',
+            description = 'An apartment so expansive, all your friends will immediately know how much you paid for it.',
+            enter = vec3(-47.52, -585.86, 37.95)
+        },
+        {
+            interior = 'RichardMajesticApt2',
+            label = 'Morningwood Blvd',
+            description = 'This breathtaking luxury condo is a stone\'s throw from AKAN Records and a Sperm Donor Clinic.',
+            enter = vec3(-936.15, -378.91, 38.96)
+        },
+        {
+            interior = 'TinselTowersApt42',
+            label = 'South Rockford Drive',
+            description = 'A picture-perfect lateral living experience in one of Los Santos most sought-after tower blocks.',
+            enter = vec3(-614.58, 46.52, 43.59)
+        },
+        {
+            interior = 'TinselTowersApt42',
+            label = 'Modern 1 Apartment',
+            description = 'An Apartment with an open view!',
+            enter = vec3(-614.58, 46.52, 43.59)
+        },
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '0.0.1'
+repository 'https://github.com/Qbox-project/qbx_properties'
+description 'Hopefully one day a feature rich property system'
+
+ox_lib 'locale'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/lib.lua',
+    'config/shared.lua',
+}
+
+client_scripts {
+    -- '@qbx_core/modules/playerdata.lua',
+    -- 'config/client.lua',
+    'client/apartmentselect.lua',
+    -- 'client/property.lua',
+    -- 'client/realtor.lua',
+    -- 'client/dataview.lua',
+    -- 'client/decorating.lua',
+}
+
+-- server_scripts {
+--     '@oxmysql/lib/MySQL.lua',
+--     'config/server.lua',
+--     'server/apartmentselect.lua',
+--     'server/property.lua',
+--     'server/realtor.lua',
+--     --'server/decorating.js' only used for taking screenshots of furniture
+-- }
+
+files {
+    'locales/*.json',
+    'screenshots/*.png'
+}
+
+lua54 'yes'
+use_experimental_fxv2_oal 'true'
